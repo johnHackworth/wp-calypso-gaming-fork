hdytsgt/wp-calypso

State: add state.ui.layoutFocus, actions, and selectors

client/state/action-types.js
@@ -124,6 +124,9 @@ export const JETPACK_SYNC_START_ERROR = 'JETPACK_SYNC_START_ERROR';
 export const JETPACK_SYNC_STATUS_REQUEST = 'JETPACK_SYNC_STATUS_REQUEST';
 export const JETPACK_SYNC_STATUS_SUCCESS = 'JETPACK_SYNC_STATUS_SUCCESS';
 export const JETPACK_SYNC_STATUS_ERROR = 'JETPACK_SYNC_STATUS_ERROR';
+export const LAYOUT_SET_FOCUS = 'LAYOUT_SET_FOCUS';
+export const LAYOUT_SET_NEXT_FOCUS = 'LAYOUT_SET_NEXT_FOCUS';
+export const LAYOUT_NEXT_FOCUS_ACTIVATE = 'LAYOUT_NEXT_FOCUS_ACTIVATE';
 export const NOTICE_CREATE = 'NOTICE_CREATE';
 export const NOTICE_REMOVE = 'NOTICE_REMOVE';
 export const OLARK_READY = 'OLARK_READY';

johnHackworth/wp-calypso-gaming-fork

client/state/action-types.js
@@ -128,6 +128,9 @@ export const JETPACK_SYNC_START_ERROR = 'JETPACK_SYNC_START_ERROR';
 export const JETPACK_SYNC_STATUS_REQUEST = 'JETPACK_SYNC_STATUS_REQUEST';
 export const JETPACK_SYNC_STATUS_SUCCESS = 'JETPACK_SYNC_STATUS_SUCCESS';
 export const JETPACK_SYNC_STATUS_ERROR = 'JETPACK_SYNC_STATUS_ERROR';
+export const LAYOUT_FOCUS_SET = 'LAYOUT_FOCUS_SET';
+export const LAYOUT_NEXT_FOCUS_SET = 'LAYOUT_NEXT_FOCUS_SET';
+export const LAYOUT_NEXT_FOCUS_ACTIVATE = 'LAYOUT_NEXT_FOCUS_ACTIVATE';
 export const NOTICE_CREATE = 'NOTICE_CREATE';
 export const NOTICE_REMOVE = 'NOTICE_REMOVE';
 export const OLARK_READY = 'OLARK_READY';