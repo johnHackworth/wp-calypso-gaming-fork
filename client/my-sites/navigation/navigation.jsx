79f9b0f939142bc5693df6d79bade0f5e9f7ab25
Payton Swick <payton@foolord.com>
2016-08-03 16:00:38 -0400

    MySitesNavigation: unconnect from Redux store

--- client/my-sites/navigation/navigation.jsx
+++ client/my-sites/navigation/navigation.jsx
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import React from 'react';
-import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
@@ -10,9 +9,6 @@ import { connect } from 'react-redux';
 import observe from 'lib/mixins/data-observe';
 import SitePicker from 'my-sites/picker';
 import Sidebar from 'my-sites/sidebar';
-import { getCurrentUser } from 'state/current-user/selectors';
-import { getCurrentLayoutFocus } from 'state/ui/layout-focus/selectors';
-import { setNextLayoutFocus, setLayoutFocus } from 'state/ui/layout-focus/actions';
 
 const MySitesNavigation = React.createClass( {
 	displayName: 'MySitesNavigation',
@@ -28,34 +24,20 @@ const MySitesNavigation = React.createClass( {
 		return (
 			<div className="sites-navigation">
 				<SitePicker
-					setNextLayoutFocus={ this.props.setNextLayoutFocus }
-					setLayoutFocus={ this.props.setLayoutFocus }
-					currentLayoutFocus={ this.props.currentLayoutFocus }
 					sites={ this.props.sites }
 					allSitesPath={ this.props.allSitesPath }
 					siteBasePath={ this.props.siteBasePath }
 					onClose={ this.preventPickerDefault }
 				/>
 				<Sidebar
-					setNextLayoutFocus={ this.props.setNextLayoutFocus }
-					setLayoutFocus={ this.props.setLayoutFocus }
 					sites={ this.props.sites }
 					allSitesPath={ this.props.allSitesPath }
 					path={ this.props.path }
 					siteBasePath={ this.props.siteBasePath }
-					currentUser={ this.props.currentUser }
 				/>
 			</div>
 		);
 	}
 } );
 
-function mapStateToProps( state ) {
-	return {
-		currentUser: getCurrentUser( state ),
-		currentLayoutFocus: getCurrentLayoutFocus( state ),
-	};
-}
-
-// TODO: set pure = true when Redux sites can be used for SitePicker
-module.exports = connect( mapStateToProps, { setNextLayoutFocus, setLayoutFocus }, null, { pure: false } )( MySitesNavigation );
+module.exports = MySitesNavigation;
